d7a0f8f562acb954618fa44d58bfdc2bf0529f13
leandrobasualdo <140207714+leandrobasualdo@users.noreply.github.com>
2023-08-27 23:42:44 -0300

    Add files via upload

---
+++ html/js/JavaScript.js
@@ -0,0 +1,17 @@
+//Ejercicio 1
+var nombre = "Leandro"
+var apellido = "Basualdo"
+
+console.log(nombre + " " + apellido);
+
+//Ejercicio 2
+var num1 = 5
+var num2 = 6
+var suma
+
+console.log(suma = num1 + num2);
+
+
+var num3 = 1
+
+console.log(suma = num1 + num2 + num3);
